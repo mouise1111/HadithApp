4c7bddb800c941533536b6fee5530fe82e0161f3
Mouise Bashir <mouise1111@gmail.com>
2023-02-22 16:05:33 +0100

    component added for hadith list

---
+++ src/routes/components/HadithSingle.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let hadiths, addToFavorites;
+</script>
+
+{#each hadiths as hadith}
+<div class="px-4 py-6 space-y-4 my-2 bg-white rounded-lg">
+    <div class="flex space-x-2 items-center">
+        <span class="font-bold">{hadith.hadithNumber}</span>
+        <span class="font-bold">Narrator:</span>
+        <span>{hadith.englishNarrator}</span>
+    </div>
+    <div class="flex flex-col space-y-2">
+        <div class="flex space-x-2 items-center">
+            <span class="font-bold">Arabic:</span>
+            <span>{hadith.hadithArabic}</span>
+        </div>
+        <div class="flex space-x-2 items-center">
+            <span class="font-bold">English:</span>
+            <span>{hadith.hadithEnglish}</span>
+        </div>
+    </div>
+    <button class="btn px-2" on:click={() => addToFavorites(hadith)}>Add to Favorites ❤️</button>
+</div>
+{/each}
