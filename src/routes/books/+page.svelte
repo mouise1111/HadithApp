a96321e0e2477a074a6d33485c8dd61f09d66a4e
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-02-23 21:32:12 +0100

    code cleanup

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,17 +1,10 @@
 <script>
-	import BookList from "../components/BookList.svelte";
+	import BookList from '../components/BookList.svelte';
 
 	export let data;
 	const { books } = data;
 </script>
 
-<<<<<<< HEAD
-<!-- 
-{#each books.books as book}
-	<h1>{book.bookName}</h1>
-{/each} -->
-=======
->>>>>>> 0bb0900c7bd48c54dee3fcf14c92f8b0cdec97bd
 <section class="px-4 py-8">
-	<BookList books={books}/>
+	<BookList {books} />
 </section>
