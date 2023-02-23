0bb0900c7bd48c54dee3fcf14c92f8b0cdec97bd
Mouise Bashir <mouise1111@gmail.com>
2023-02-23 21:27:38 +0100

    Moved code to a component - booklist

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,30 +1,10 @@
 <script>
+	import BookList from "../components/BookList.svelte";
+
 	export let data;
 	const { books } = data;
 </script>
 
-<!-- 
-{#each books.books as book}
-	<h1>{book.bookName}</h1>
-{/each} -->
-
 <section class="px-4 py-8">
-	<div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-		{#each books.books as book}
-			<a
-				href="/books/{book.bookSlug}"
-				class="bg-white block p-4 rounded-lg shadow-md transition-all hover:shadow-xl"
-			>
-				<div class="text-lg font-medium text-primary">{book.bookName}</div>
-				<div class="text-sm text-secon">{book.writerName}</div>
-				<div class="mt-4">
-					<div class="text-sm font-medium text-secondary">Stats</div>
-					<div class="mt-2 flex items-center text-sm">
-						<span class="mr-4"><strong>{book.hadiths_count}</strong> Hadiths</span>
-						<span><strong>{book.chapters_count}</strong> Chapters</span>
-					</div>
-				</div>
-			</a>
-		{/each}
-	</div>
+	<BookList books={books}/>
 </section>
