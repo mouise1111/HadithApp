9d89d0f14c56833476c839186da3e3054086d4da
Mouise Bashir <mouise1111@gmail.com>
2023-03-01 10:04:32 +0100

    minor code change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,6 @@
 <script>
-	import { writable } from 'svelte/store';
 	import { favorites } from '../stores/favorites';
-
-	// function handleKeyDown(event) {
-	// 	if ((event.key === 'Enter')) {
-	// 		console.log('enter key pressed ');
-	// 		addFruit(name)
-	// 	}
-	// }
-	// let name = '';
+	import { slide } from 'svelte/transition';
 
 	let hadith_values;
 	favorites.subscribe((value) => {
@@ -16,15 +8,20 @@
 	});
 
 	function removeFromFavorites(hadith) {
-		// remove hadith from favorites
+		favorites.update(fav => fav.filter(h => h !== hadith));
 	}
+
+	console.log(hadith_values);
 </script>
 
-<h1 class="text-5xl font-bold text-center m-3 mb-5">Hadith.com</h1>
-{#if hadith_values != null}
-	<h2 class="text-xl font-bold m-3 text-primary">Your favorite hadiths</h2>
+<h1 class="text-5xl font-bold text-center m-3 mb-5">Welcome to Hadith.com</h1>
+<p class="txt text-primary text-center">
+	The Hadith of the Prophet Muhammad (صلى الله عليه و سلم) at your fingertips
+</p>
+{#if hadith_values.length != 0}
+	<h2 class="text-xl font-bold m-3 text-primary mx-5">Your favorite hadiths</h2>
 	{#each hadith_values as hadith}
-		<div class="px-4 py-6 space-y-4 my-2 bg-white rounded-lg">
+		<div class="px-4 py-6 space-y-4 my-2 bg-white rounded-lg" transition:slide|local>
 			<div class="flex space-x-2 items-center">
 				<span class="font-bold">{hadith.hadithNumber}</span>
 				<span class="font-bold">Narrator:</span>
@@ -47,14 +44,17 @@
 	{/each}
 {/if}
 
-{#if hadith_values === null}
-	<h2 class="text-xl font-bold m-3 text-primary">You don't have any favorite hadiths yet...</h2>
+{#if hadith_values.length === 0}
+<div class="m-8">
+	<h2 class="text-xl font-bold text-secondary">You don't have any favorite hadiths yet...</h2>
+	<p class="py-5">
+		To add hadith to favorites, go <a href="/books" class="link">here</a>
+	</p>
+</div>
 {/if}
 <!-- <HadithSingle hadith={hadith_values} addToFavorites={'a'} /> -->
 
-<!-- <input type="text" placeholder="Type fruit here" class="input w-full max-w-xs" bind:value={name} on:keydown={handleKeyDown}> -->
 
-<!-- <button class="btn btn-primary" on:click={addFruit(name)}>add to basket</button> -->
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
--- src/routes/books/+layout.svelte
+++ src/routes/books/+layout.svelte
@@ -1,9 +1,9 @@
-<div class="text-sm breadcrumbs text-neutral px-5">
+<!-- <div class="text-sm breadcrumbs text-neutral px-5">
 	<ul>
 		<li><a href="/books">Books</a></li>
 		<li><a href="/books/">Chapters</a></li>
 		<li>Add Document</li>
 	</ul>
-</div>
+</div> -->
 
 <slot />
